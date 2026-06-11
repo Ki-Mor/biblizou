1db8600643ee7cacc1015d1f7d23c67db38e49f5
Ki-mor <francois.botcazou@proton.me>
2026-06-11 21:21:12 +0200

    ajout de la méthode def setup_collapsible_groupbox qui replie les groupes décochés

--- biblizou_dialog_patri.py
+++ biblizou_dialog_patri.py
@@ -18,3 +18,21 @@ class BiblizouDialogPatri(QtWidgets.QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         super(BiblizouDialogPatri, self).__init__(parent)
         self.setupUi(self)
+        self.setup_collapsible_groupbox(self.gBConventionInternationales)
+        self.setup_collapsible_groupbox(self.gBDirEuro)
+        self.setup_collapsible_groupbox(self.gBProtection)
+        self.setup_collapsible_groupbox(self.gBListesRouges)
+
+    def setup_collapsible_groupbox(self, groupbox):
+        groupbox.setCheckable(True)
+        groupbox.initial_max_height = groupbox.maximumHeight()
+
+        def toggle_group(is_checked):
+            if is_checked:
+                groupbox.setMaximumHeight(
+                    groupbox.initial_max_height if groupbox.initial_max_height != 16777215 else 1000)
+            else:
+                groupbox.setMaximumHeight(24)
+
+        groupbox.toggled.connect(toggle_group)
+        toggle_group(groupbox.isChecked())
