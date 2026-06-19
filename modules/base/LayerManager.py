2e8df327ab3b6da34012f57d9ce6378dbb250a23
Ki-Mor <francois.botcazou@proton.me>
2026-06-19 14:27:39 +0200

    feat: initialiser le fichier LayerManager.py (#29)

---
+++ modules/base/LayerManager.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Auteur : ExEco Environnement - François Botcazou
+Date : 2026/06
+Version : 1.0
+Nom : LayerManager.py
+Groupe : base
+Description : Classe utilitaire centralisant la gestion, la validation, le chargement
+              et la substitution des couches vectorielles (QgsVectorLayer) au sein du projet QGIS.
+"""
