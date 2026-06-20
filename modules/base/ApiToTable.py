97445d7915a76b2e3036833d8c059dbe2b16bd35
Ki-mor <francois.botcazou@proton.me>
2026-06-20 17:29:04 +0200

    suppression du script vide
    Signed-off-by: Ki-mor <francois.botcazou@proton.me>

--- modules/base/ApiToTable.py
+++
@@ -1,8 +0,0 @@
-"""
-Auteur : ExEco Environnement - François Botcazou
-Nom : ApiToTable.py
-Groupe : base
-Description : Classe abstraite (ABC) mutualisée pour l'extraction de données
-              à partir de l'API de l'INPN vers une table.
-"""
-
