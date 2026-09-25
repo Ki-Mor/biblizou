f5d1e3b05d87ccf0755f47ccea214dfd288e68f8
Ki-Mor <francois.botcazou@proton.me>
2026-09-25 16:06:32 +0200

    feat: ajoute les méthodes statiques _get_table_data et _add_row pour refacto add_taxref_row ; get_taxref_consolidation_data ; add_stat_row(self) ; get_stat_config dans biblizou_dockwidget.py

--- modules/base/UiUtils.py
+++ modules/base/UiUtils.py
@@ -1,4 +1,41 @@
 @staticmethod
+def _add_row(table):
+    """Ajoute une ligne au tables de l'ui."""
+    row = table.rowCount()
+    table.insertRow(row)
+
+    lyr_cb = QgsMapLayerComboBox()
+    lyr_cb.setFilters(QgsMapLayerProxyModel.VectorLayer)
+    fld_cb = QgsFieldComboBox()
+    fld_cb.setLayer(lyr_cb.currentLayer())
+    lyr_cb.layerChanged.connect(fld_cb.setLayer)
+
+    btn_del = QPushButton()
+    icon_path = os.path.join(os.path.dirname(__file__), 'misc', 'cross.png')
+    btn_del.setIcon(QtGui.QIcon(icon_path))
+    btn_del.setIconSize(QtCore.QSize(16, 16))
+    btn_del.setMaximumWidth(30)
+    btn_del.clicked.connect(lambda: self.tableTaxref.removeRow(self.tableTaxref.indexAt(btn_del.pos()).row()))
+
+    table.setCellWidget(row, 0, lyr_cb)
+    table.setCellWidget(row, 1, fld_cb)
+    table.setCellWidget(row, 2, btn_del)
+
+@staticmethod
+def _get_table_data(table):
+    """Extrait les IDs et colonnes du tableau (pour collecte cd_nom)."""
+    data = []
+    for row in range(table.rowCount()):
+        lyr_widget = table.cellWidget(row, 0)
+        fld_widget = table.cellWidget(row, 1)
+        if lyr_widget and lyr_widget.currentLayer():
+            data.append({
+                "layer_id": lyr_widget.currentLayer().id(),
+                "column": fld_widget.currentField()
+            })
+    return data
+
+@staticmethod
 def _auto_lookup_layer(iface) -> tuple[List[QgsVectorLayer], List[str]] | None:
     """
     Recherche automatiquement les couches et les en-têtes de colonnes
